438d57010c85151656685caa76b652c928958d07
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2025-05-12 02:37:20 +0200

    Update visual_monte_carlo_history.cpp

--- codiciTesi/visual_monte_carlo_history.cpp
+++ codiciTesi/visual_monte_carlo_history.cpp
@@ -18,49 +18,159 @@ void plot_points(vector<double>& x, vector<double>& y, string name_image, string
 
 int main() {
 
-	int skipLines = 1; //= number of lines to skip while reading input file;
-	vector<double> time, polyr_vec, polyi_vec; //to contain data
+	int skipLines_file_list = 1, skipLines = 1; //= number of lines to skip while reading input file;
+	vector<double> time1, time2, polyr_vec, polyi_vec, reff_vec, imff_vec; //to contain data
 	int index = 0, conf_id;
-	double value_tmp, poly_re, poly_im;
+	double value_tmp, poly_re, poly_im, ff_re, ff_im;
 	string line;
+	vector<string> directories;
+	vector<string> gauge_files;
+	vector<string> fermion_files;
+	string polyre_image;
+	string polyim_image;
+	string y_name1;
+	string y_name2;
+	string reff_image;
+	string imff_image;
+	string title;
+	string name_file_list = "11_05_2025/file_list.txt";
 	
-	string directory = "11_05_2025/build_good_001_out/";
-	string name_input_file = "gauge_obs3725763671.txt";
-	name_input_file = directory + name_input_file;
-	string name_image1 = "gauge_obs3725763671.png";
-	string title = "Monte Carlo History";
-	title = title + " gauge_obs3725763671:";
-	string y_name1 = "Re{P}";
-	string y_name2 = "Im{P}";
-	string name_image2 = y_name2 + "_" + name_image1;
-	name_image1 = y_name1 + "_" + name_image1;
-
-	ifstream input_file; //declaration of input file
-
-	input_file.open(name_input_file);
-	if (!input_file) {
-		cout << "Error opening input file" << endl;
+	ifstream file_list;
+	file_list.open(name_file_list);
+	if (!file_list) {
+		cout << "Error opening file list" << endl;
 		return 1;
 	}
 
 	for (int i = 0; i < skipLines; i++) {
-		if (!getline(input_file, line)) {
-			cerr << "Error: there are less than " << skipLines << " lines in the file." << endl;
+		if (!getline(file_list, line)) {
+			cerr << "Error: there are less than " << skipLines_file_list << " lines in the file list." << endl;
 			return 1;
 		}
 	}
-
-	while (input_file >> conf_id >> value_tmp >> value_tmp >> value_tmp >> poly_re >> poly_im) {//DEVI CORREGGEERE ERRORI
-		time.push_back((double)conf_id);
-		polyr_vec.push_back(poly_re);
-		polyi_vec.push_back(poly_im);
+	
+	while (getline(file_list, line)) {
+		istringstream iss(line);
+		string dir, gauge, ferm;
+		if (iss >> dir >> gauge >> ferm) {
+			directories.push_back(dir);
+			gauge_files.push_back(gauge);
+			fermion_files.push_back(ferm);
+		}
+		else {
+			cerr << "Poorly formatted line: " << line << endl;
+		}
 	}
 
-	input_file.close();
+	file_list.close();
 
-	//GRAPHIC REPRESENTATION:
-	plot_points(time, polyr_vec, name_image1, title, y_name1, 0.47);
-	plot_points(time, polyi_vec, name_image2, title, y_name1, 0.47);
+	size_t pos;
+	string name_tmp;
+	for (int ii = 0; ii < directories.size(); ii++) {
+		
+		y_name1 = "Re{P}";
+		y_name2 = "Im{P}";
+
+		ifstream input_file; //declaration of input file
+		input_file.open(directories[ii] + gauge_files[ii]);
+		if (!input_file) {
+			cout << "Error opening " << gauge_files[ii] << endl;
+			return 1;
+		}
+		
+		pos = gauge_files[ii].find_last_of(".");
+		if (pos != std::string::npos) {
+			gauge_files[ii] = gauge_files[ii].substr(0, pos); //I remove extension using substr
+		}
+		title = "Monte Carlo History " + gauge_files[ii] + ":";
+		polyre_image = y_name1 + "_" + gauge_files[ii] + ".png";
+		polyim_image = y_name2 + "_" + gauge_files[ii] + ".png";
+
+		for (int i = 0; i < skipLines; i++) {
+			if (!getline(input_file, line)) {
+				cerr << "Error: there are less than " << skipLines << " lines in the file: " << gauge_files[ii] << endl;
+				return 1;
+			}
+		}
+
+		while (getline(input_file, line)) {
+			if (line.find_first_not_of(" \t\r\n") == string::npos) continue;//skips empty lines or lines composed only of spaces/tabs
+
+			istringstream iss(line);
+			if (iss >> conf_id >> value_tmp >> value_tmp >> value_tmp >> poly_re >> poly_im) {
+				time1.push_back(static_cast<double>(conf_id));
+				polyr_vec.push_back(poly_re);
+				polyi_vec.push_back(poly_im);
+			}
+			else {
+				cerr << "Skipped gauge line (badly formatted): " << line << endl;
+			}
+		}
+
+		input_file.close();
+
+		//GRAPHIC REPRESENTATION:
+		plot_points(time1, polyr_vec, polyre_image, title, y_name1, 0.47);
+		cout << ii << ": Re{P} image DONE!" << endl;
+		plot_points(time1, polyi_vec, polyim_image, title, y_name1, 0.47);
+		cout << ii << ": Im{P} image DONE!" << endl;
+		time1.clear();
+		polyr_vec.clear();
+		polyi_vec.clear();
+	}
+
+	for (int ii = directories.size(); ii < 2*directories.size(); ii++) {
+
+		index = ii - directories.size();
+		y_name1 = "Re{ff}";
+		y_name2 = "Im{ff}";
+
+		ifstream input_file; //declaration of input file
+		input_file.open(directories[index] + fermion_files[index]);
+		if (!input_file) {
+			cerr << "Error opening " << fermion_files[index] << endl;
+			return 1;
+		}
+
+		pos = fermion_files[index].find_last_of(".");
+		if (pos != std::string::npos) {
+			fermion_files[index] = fermion_files[index].substr(0, pos); //I remove extension using substr
+		}
+		title = "Monte Carlo History " + fermion_files[index] + ":";
+		reff_image = y_name1 + "_" + fermion_files[index] + ".png";
+		imff_image = y_name2 + "_" + fermion_files[index] + ".png";
+
+		for (int i = 0; i < skipLines; i++) {
+			if (!getline(input_file, line)) {
+				cerr << "Error: there are less than " << skipLines << " lines in the file" << fermion_files[index] << endl;
+				return 1;
+			}
+		}
+
+		while (getline(input_file, line)) {
+			istringstream iss(line);
+			if (iss >> conf_id >> value_tmp >> value_tmp >> value_tmp >> ff_re >> ff_im) { //read the line only until ff_im: if next elements are omitted isn't interesting.
+				if (line.find_first_not_of(" \t\r\n") == string::npos) continue;
+				time2.push_back(static_cast<double>(conf_id));
+				reff_vec.push_back(ff_re);
+				imff_vec.push_back(ff_im);
+			}
+			else {
+				cerr << "Skipped line (badly formatted): " << line << endl;
+			}
+		}
+
+		input_file.close();
+
+		//GRAPHIC REPRESENTATION:
+		plot_points(time2, reff_vec, reff_image, title, y_name1, 0.47);
+		cout << index << ": Re{ff} image DONE!" << endl;
+		plot_points(time2, imff_vec, imff_image, title, y_name1, 0.47);
+		cout << index << ": Im{ff} image DONE!" << endl;
+		time2.clear();
+		reff_vec.clear();
+		imff_vec.clear();
+	}
 
 	return 0;
 }
@@ -91,11 +201,12 @@ void plot_points(vector<double>& x, vector<double>& y, string name_image, string
 	g->SetLineColor(1);
 
 	g->GetYaxis()->SetRangeUser(min_y - 0.01 * abs(min_y), max_y + 0.01 * abs(max_y));//to set y axis range
-	g->GetXaxis()->SetRangeUser(min_x - 0.01 * abs(min_x), max_x);//to set x axis range
+	g->GetXaxis()->SetLimits(min_x- 100, max_x + 100);//to set x axis range //SetLimits cannot be ignored by ROOT, while SetRangeUser yes.
 
 	gStyle->SetOptFit(1111);
 
 	g->Draw("ALP");
+	gPad->Update();
 
 	//LATEX: I add LaTeX titles and axis labels:
 	TLatex latex;
