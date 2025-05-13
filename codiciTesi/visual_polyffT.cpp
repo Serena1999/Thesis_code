32dcd4cb0614b5e24e0d65fb698e3c984bc36bc2
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2025-05-13 17:56:43 +0200

    visualizzazione versus temperatura
    al momento senza barre d'errore

---
+++ codiciTesi/visual_polyffT.cpp
@@ -0,0 +1,203 @@
+﻿/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+****                    visual_polyffT.cpp:                   ****
+****    visualization of the mean values ​​of the observables   **** 
+****          (with corresponding standard deviation)         ****
+****             as a function of the temperature             ****
+****                (author = Serena Bruzzesi)                ****
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+
+#include "../library.h"
+#include "../root_include.h"
+
+const string tipology = "fermion"; //gauge/fermion, CHOOSABLE --> to do the gauge/fermion observables graph
+
+//-----------------------------------------------------------------
+//ROOT MACRO TO DO FIT AND GRAPH:
+
+void plot_points(vector<double>& x, vector<double>& y, string name_image, string title, string y_name, double pos_title, double pos_y, double heigh_y);
+
+//-----------------------------------------------------------------
+//MAIN:
+
+int main() {
+
+	int skipLines = 1; //= number of lines to skip while reading input file;
+	//vector<double> time1, time2, polyr_vec, polyi_vec, reff_vec, imff_vec; //to contain data
+	//int index = 0, conf_id;
+	//double value_tmp, poly_re, poly_im, ff_re, ff_im;
+	double temp_value, mod_value, mod_err_value, re_value, re_err_value, im_value, im_err_value;
+	vector <double> temp, mod, mod_err, re, re_err, im, im_err;
+	size_t pos;
+	string line, name_tmp;
+	string input_directory = "11_05_2025/polyff_results/";
+	string output_directory = "results/";
+	string name_input_file;
+	pos = name_input_file.find_last_of(".");
+	if (pos != std::string::npos) {
+		name_tmp = name_input_file.substr(0, pos); //I remove extension using substr
+	}
+	string name_image_mod;
+	string name_image_re;
+	string name_image_im;
+	string title_mod;
+	string title_re;
+	string title_im;
+	string y_name_mod;
+	string y_name_re;
+	string y_name_im;
+	double pos_ymod;
+	double pos_yre;
+	double pos_yim;
+	double height_mod;
+	double height_re;
+	double height_im;
+	double pos_title_mod;
+	double pos_title_re;
+	double pos_title_im;
+
+	if (tipology == "gauge") {
+		name_input_file = "800.0_poly_results.txt";
+		pos = name_input_file.find_last_of(".");
+		if (pos != string::npos) {
+			name_tmp = name_input_file.substr(0, pos); //I remove extension using substr
+		}
+		name_input_file = input_directory + name_input_file;
+		name_image_mod = output_directory + "modPvsT_" + name_tmp + ".png";
+		name_image_re = output_directory + "rePvsT_" + name_tmp + ".png";
+		name_image_im = output_directory + "imPvsT_" + name_tmp + ".png";
+		title_mod = "<|<P* P^dag>|> vs temperature:";
+		title_re = "<Re{P}> vs temperature:";
+		title_im = "<Im{P}> vs temperature:";
+		y_name_mod = "<|<P* P^dag>|>";
+		y_name_re = "<Re{P}>";
+		y_name_im = "<Im{P}>";
+		pos_ymod = 0.02;
+		pos_yre = 0.03;
+		pos_yim = 0.03;
+		height_mod = 0.4;
+		height_re = 0.45;
+		height_im = 0.45;
+		pos_title_mod = 0.3;
+		pos_title_re = 0.3;
+		pos_title_im = 0.3;
+	}
+	else if (tipology == "fermion") {
+		name_input_file = "800.0_ff_results.txt";
+		pos = name_input_file.find_last_of(".");
+		if (pos != string::npos) {
+			name_tmp = name_input_file.substr(0, pos); //I remove extension using substr
+		}
+		name_input_file = input_directory + name_input_file;
+		name_image_mod = output_directory + "modffvsT_" + name_tmp + ".png";
+		name_image_re = output_directory + "reffvsT_" + name_tmp + ".png";
+		name_image_im = output_directory + "imffvsT_" + name_tmp + ".png";
+		title_mod = "<|<ff* ff^dag>|> vs temperature:";
+		title_re = "<Re{ff}> vs temperature:";
+		title_im = "<Im{ff}> vs temperature:";
+		y_name_mod = "<|<ff* ff^dag>|>";
+		y_name_re = "<Re{ff}>";
+		y_name_im = "<Im{ff}>";
+		pos_ymod = 0.03;
+		pos_yre = 0.03;
+		pos_yim = 0.04;
+		height_mod = 0.4;
+		height_re = 0.45;
+		height_im = 0.45;
+		pos_title_mod = 0.3;
+		pos_title_re = 0.3;
+		pos_title_im = 0.3;
+	}
+	else {
+		cerr << "Invalid tipology: you must write gauge or fermion";
+		return 1;
+	}
+
+	ifstream input_file;
+	input_file.open(name_input_file);
+	if (!input_file) {
+		cerr << "Error opening input file." << endl;
+		return 1;
+	}
+
+	for (int i = 0; i < skipLines; i++) {
+		if (!getline(input_file, line)) {
+			cerr << "Error: there are less than " << skipLines << " lines in the input file." << endl;
+			return 1;
+		}
+	}
+
+	while (getline(input_file, line)) {
+		istringstream iss(line);
+		if (iss >> temp_value >> mod_value >> mod_err_value >> re_value >> re_err_value >> im_value >> im_err_value) {
+			temp.push_back(temp_value);
+			mod.push_back(mod_value);
+			mod_err.push_back(mod_err_value);
+			re.push_back(re_value);
+			re_err.push_back(re_err_value);
+			im.push_back(im_value);
+			im_err.push_back(im_err_value);
+		}
+		else {
+			cerr << "Poorly formatted line: " << line << endl;
+		}
+	}
+
+	input_file.close();
+
+	//GRAPHIC REPRESENTATION:
+	plot_points(temp, mod, name_image_mod, title_mod, y_name_mod, pos_title_mod, pos_ymod, height_mod);
+	plot_points(temp, re, name_image_re, title_re, y_name_re, pos_title_re, pos_yre, height_re);
+	plot_points(temp, im, name_image_im, title_im, y_name_im, pos_title_im, pos_yim, height_im);
+
+	return 0;
+}
+
+//-----------------------------------------------------------------
+//ROOT MACRO TO GRAPH:
+
+void plot_points(vector<double>& x, vector<double>& y, string name_image, string title, string y_name, double pos_title, double pos_y, double heigh_y) {
+
+	//CANVAS creation: (to draw the graph)
+	TCanvas* canvas = new TCanvas("canvas", "Canvas for Drawing Points", 900, 600);
+	canvas->SetGrid();//to set grid
+
+	TGraph* g = new TGraph(y.size(), x.data(), y.data());
+	//Using std::max_element: (useful to create my histogram object)
+	//	-> std::max_element takes two iterators that define the range of the array to operate on. 
+	//	-> returns an iterator that points to the maximum element found.
+	//The usage of std::min_element is analogous but with for minimum element.
+	auto min_x = *min_element(x.begin(), x.end());
+	auto max_x = *max_element(x.begin(), x.end());
+	auto min_y = *min_element(y.begin(), y.end());
+	auto max_y = *max_element(y.begin(), y.end());
+
+	g->SetTitle(""); //to clear default title
+	g->GetXaxis()->SetTitle(""); //to clear default X axis title
+	g->GetYaxis()->SetTitle(""); //to clear default Y axis title
+
+	g->SetLineColor(1);
+
+	g->GetYaxis()->SetRangeUser(min_y - 0.01 * abs(min_y), max_y + 0.01 * abs(max_y));//to set y axis range
+	g->GetXaxis()->SetLimits(min_x, max_x);//to set x axis range //SetLimits cannot be ignored by ROOT, while SetRangeUser yes.
+	g->SetMarkerStyle(20);
+	g->Draw("ALP");
+	gPad->Update();
+
+	//LATEX: I add LaTeX titles and axis labels:
+	TLatex latex;
+	latex.SetNDC(); //sets the use of Normalized Device Coordinates (NDC).
+	latex.SetTextSize(0.05); //changes text size for title
+	latex.DrawLatex(pos_title, 0.92, title.c_str());
+	latex.SetTextSize(0.04); //changes text size for axis labels
+	latex.DrawLatex(0.45, 0.03, "T[MeV]");
+	latex.SetTextAngle(90);
+	latex.DrawLatex(pos_y, heigh_y, y_name.c_str());
+
+
+	//SAVE: I save the canvas as an image
+	canvas->SaveAs(name_image.c_str());
+
+	//DELETE:
+	delete g;
+	delete canvas;
+}
