9ee2cd915d49fdd180b9260d430595e78e6aac08
Serena Bruzzesi <48393608+Serena1999@users.noreply.github.com>
2025-06-30 19:42:11 +0200

    Update rho_vs_T.cpp
    analysis_rhok_vs_T.cpp ultimated

--- codiciTesi/rho_vs_T.cpp
+++ codiciTesi/rho_vs_T.cpp
@@ -1,13 +1,9 @@
 /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 ****                  analysis_rhok_vs_T.cpp:                 ****
-**** Starting from dedicated files ("corrected_mon.dat"), the ****
-**** number nk of monopoles is calculated for each number k of****
-****  windings, for each gauge configuration. Then, averaging ****
-****  over all configurations, the mean of nk (at a given k), ****
-****    the mean of the ratio between nk and n1, the ratio    ****
-****  between the means of nk and n1 are estimated. Two plots ****
-****   are returned for the last 2 quantities mentioned as a  ****
-****                      function of k.                      ****
+****   Starting from dedicated files ("corrected_mon.dat"),   ****
+****    computes total monopole density per configuration.    ****
+****    Averages over configurations at each temperature T.   ****
+****    Plots <rho_tot/T^3> with its error vs temperature.    ****
 ****                (author = Serena Bruzzesi)                ****
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 
@@ -17,14 +13,10 @@
 #include "../library.h"//DA STUDIARE BENE!!
 #include "../root_include.h"
 
-//IMPORTANT CONSIDERATIONS:
-// -> BLOCKING SEEMS UNECESSARY FROM AUTOCORR_CHECK_RHOK.CPP (TO VERIFY BETTER)... SO FOR NOW I IMPLEMENT THE INDEPENDENT ESTIMATE OF VARIANCE FOR <nk/n1> 
-
 //-----------------------------------------------------------------
 //GLOBAL CONSTANTS:
 
 const double hbar_c = 197.3269804; //MeV * fm
-
 
 //-----------------------------------------------------------------
 //ROOT MACRO TO DO FIT AND GRAPH:
@@ -66,10 +58,6 @@ void read_file_list_DIRECTORIES_THERM(//SCRIVI SOTTO COME IMPLEMENTARLA, PRENDI 
 	int step_sample_fermion
 );
 
-double f(double rho_k, double rho_1) {
-	return rho_k / rho_1;
-}
-
 //-----------------------------------------------------------------
 //MAIN:
 
@@ -82,7 +70,6 @@ int main() {
 	int Nt = 8;
 	int Ns = 32;
 	int ad_Vs = Ns * Ns * Ns; //adimensional spatial volume
-	int Vs; //it will be the dimensional spatial volume = Vs*a^3
 
 	string list_file = "11_05_2025/file_list_therm_extended.txt";
 	int skipLines_list_file = 1;
@@ -110,10 +97,6 @@ int main() {
 		temp
 	);
 
-
-	int n_boot_steps = 100; //for estimate errors of secondary observables with Bootstrap.
-	int seed = 1; //for estimate errors of secondary observables with Bootstrap.
-
 	vector <double> mean_rho_T3;
 	/*
 	-> mean_rho_T3[ii] = mean of rho_tot/T^3 over configurations = <rho_tot>/T^3
@@ -121,19 +104,20 @@ int main() {
 	vector <double> mean_2; //mean of squares
 	vector <double> err_rho_T3; //standard deviation from the mean
 
-	string name_image, name_output_file;
+	string name_image = "results/rho_tot_mpi" + mpi + ".png";
+	string name_output_file = "results/rho_tot_mpi" + mpi + ".txt";
 
 	string title = "#LT #rho / V_{s}^{3} #GT vs temperature:";
 	
 	string y_name = "#LT #rho / V_{s}^{3} #GT";
 
-	double pos_title = 0.5;
+	double pos_title = 0.3;
 	
-	double pos_y = 0.020;
+	double pos_y = 0.04;
 	
-	double heigh_y = 0.45;
+	double heigh_y = 0.4;
 	
-	double width_canvas = 900;
+	double width_canvas = 800;
 	
 	read_file_list_DIRECTORIES_THERM(
 		list_file,
@@ -144,12 +128,13 @@ int main() {
 		step_sample_fermion
 	);
 
-	sample_gen sampler;
-	sampler.init(seed);
-
 	for (int ii = 0; ii < directories.size(); ++ii) {
+
+		mean_rho_T3.push_back(0);
+		mean_2.push_back(0);
+
 		ifstream input_file;
-		input_file.open(directories[ii] + "rho_tot_" + name_input_file);
+		input_file.open(directories[ii] + name_input_file);
 		if (!input_file) {
 			cerr << "Error opening input " << ii << "-th file." << endl;
 			return 1;
@@ -188,10 +173,9 @@ int main() {
 
 		//for each configurazion, we compute the densities and then we do the mean:
 		double discard1, discard2, discard3, discard4, discard5, discard6, discard7, wrap_value;
-		double n_accum = 0;
 		int n_conf = 0;
-		vector <double> value_rhok;
-		double mean_rho_T3_value;
+		vector <double> nk;
+		double rho_T3_value;
 		
 		while (getline(input_file, line)) {
 			istringstream iss(line);
@@ -212,85 +196,50 @@ int main() {
 					//cout << discard7 << endl;
 					//cout << wrap_value << endl;
 					//cout << endl;
-					if (value_rhok.empty()) {
+					if (nk.empty()) {
 						cout << "No monopoles for conf_id = " << to_string(conf_tmp) << endl;
 						cout << "--> so we omit this configuration." << endl;
 						conf_tmp = conf_id;
 					}
 					else {
 						++n_conf;
-						mean_rho_T3_value = 0;
-						for (int kk = 0; kk < value_rhok.size(); ++kk) {
-							mean_rho_T3_value += kk * value_rhok[kk];
-							mean_rho_T3.push_back();
-							if (kk >= mean_rhok.size()) {
-								mean_rhok.resize(kk + 1, 0.0);
-								mean0_2.resize(kk + 1, 0.0);
-								mean_rhok_rho1.resize(kk + 1, 0.0);
-								mean1_2.resize(kk + 1, 0.0);
-							}
-							if (kk >= matrix_value_rhok.size()) {
-								matrix_value_rhok.resize(kk + 1, vector<double>());
-							}
-							matrix_value_rhok[kk].push_back(value_rhok[kk]);
-							value_rhok[kk] /= n_accum;
-							mean_rhok[kk] += value_rhok[kk];
-							mean0_2[kk] += (value_rhok[kk] * value_rhok[kk]);
-							value_rhok_rho1[kk] /= n_accum;
-							mean_rhok_rho1[kk] += value_rhok_rho1[kk];
-							mean1_2[kk] += (value_rhok_rho1[kk] * value_rhok_rho1[kk]);
+						rho_T3_value = 0;
+						for (int kk = 0; kk < nk.size(); ++kk) {
+							rho_T3_value += kk * nk[kk];
 						}
-						mean_rho_T3_value /= (pow(afm[ii], 3) * Vs);//AGGIUSTA DIMENSIONI
+						mean_rho_T3[ii] += rho_T3_value;
+						mean_2[ii] += (rho_T3_value * rho_T3_value);
 					}
-					value_rhok.clear();
-					value_rhok_rho1.clear();
-					n_accum = 0;
+					nk.clear();
 					conf_tmp = conf_id;
 				}
 				if (wrap_value == 0) {
 					continue;
 				}
-				if (abs(wrap_value) > value_rhok.size()) {
-					value_rhok.resize(abs(wrap_value), 0.0);
-					value_rhok_rho1.resize(abs(wrap_value), 0.0);
+				if (abs(wrap_value) > nk.size()) {
+					nk.resize(abs(wrap_value), 0.0);
 				}
-				++n_accum;
-				value_rhok[abs(wrap_value) - 1] += 1;
+				nk[abs(wrap_value) - 1] += 1;
 			}
 			else {
 				cerr << "Skipped line (badly formatted) from" << (ii + 1) << "-th input file: " << line << endl;
 			}
 		}
 		if (conf_id == conf_tmp) {
-			if ((value_rhok.empty()) || (value_rhok[0] == 0)) {
-				cout << "No monopoles with only one wrapping for conf_id = " << to_string(conf_tmp) << endl;
+			if (nk.empty()) {
+				cout << "No monopoles for conf_id = " << to_string(conf_tmp) << endl;
 				cout << "--> so we omit this configuration." << endl;
 			}
 			else {
 				++n_conf;
-				for (int kk = 0; kk < value_rhok.size(); ++kk) {
-					value_rhok_rho1[kk] = value_rhok[kk] / (double)value_rhok[0];
-					//cout << "value_rhok_rho1.size() = " << value_rhok_rho1.size() << endl;
-					if (kk >= mean_rhok.size()) {
-						mean_rhok.resize(kk + 1, 0.0);
-						mean0_2.resize(kk + 1, 0.0);
-						mean_rhok_rho1.resize(kk + 1, 0.0);
-						mean1_2.resize(kk + 1, 0.0);
-					}
-					if (kk >= matrix_value_rhok.size()) {
-						matrix_value_rhok.resize(kk + 1, vector<double>());
-					}
-					matrix_value_rhok[kk].push_back(value_rhok[kk]);
-					value_rhok[kk] /= n_accum;
-					mean_rhok[kk] += value_rhok[kk];
-					mean0_2[kk] += (value_rhok[kk] * value_rhok[kk]);
-					value_rhok_rho1[kk] /= n_accum;
-					mean_rhok_rho1[kk] += value_rhok_rho1[kk];
-					mean1_2[kk] += (value_rhok_rho1[kk] * value_rhok_rho1[kk]);
+				rho_T3_value = 0;
+				for (int kk = 0; kk < nk.size(); ++kk) {
+					rho_T3_value += kk * nk[kk];
 				}
+				mean_rho_T3[ii] += rho_T3_value;
+				mean_2[ii] += (rho_T3_value * rho_T3_value);
 			}
-			value_rhok.clear();
-			value_rhok_rho1.clear();
+			nk.clear();	
 			//if (ii == 12) {
 			//	cout << endl;
 			//	//cout << kk << endl;
@@ -299,182 +248,91 @@ int main() {
 			//}
 		}
 
+		if (n_conf == 0) {
+			cout << "No configuration considerd for " << to_string(ii + 1) << "-th temperature: T = " << temp[ii] << endl;
+			continue;
+		}
+
+		double factor = pow(Nt, 3) / ad_Vs;
+		/* 
+		look at formula 32 of page 10 of second article given by professor.
+		*/
+
+		mean_rho_T3[ii] /= (double)n_conf;
+		mean_2[ii] /= (double)n_conf;
+
+		mean_rho_T3[ii] *= factor;
+		mean_2[ii] *= factor;
+
+		err_rho_T3.push_back(mean_2[ii] - mean_rho_T3[ii] * mean_rho_T3[ii]);
+		
+		if (n_conf == 1) {
+			cout << "No sufficient configurations to evaluete error for configuration of " << to_string(ii + 1) << "-th temperature: T = " << temp[ii] << endl;
+			continue;
+		}
+		
+		err_rho_T3[ii] /= (double)(n_conf - 1);
+
+		if (err_rho_T3[ii] < 0) {
+			cout << "Negative variance for configuration of " << to_string(ii + 1) << "-th temperature: T = " << temp[ii] << endl;
+			continue;
+		}
+
+		if (isnan(err_rho_T3[ii])) {
+			cout << "Nan variance for configuration of " << to_string(ii + 1) << "-th temperature: T = " << temp[ii] << endl;
+			continue;
+		}
+
+		err_rho_T3[ii] = sqrt(err_rho_T3[ii]);
+
 		input_file.close();
 
-		cout << mean_rhok.size() << endl;
-		cout << mean_rhok_rho1.size() << endl;
+		cout << mean_rho_T3[ii] << endl;
+		cout << mean_2[ii] << endl;
 		cout << n_conf << endl;
-
-		if (mean_rhok.size() <= 1) {
-			cout << "No monopoles with more than 1 wrapping in " << ii << "-th iteration" << endl;
-			cout << "Not considered " << ii << "-th iteration" << endl;
-			continue;
-		}
-
-		if (mean_rhok[0] == 0) {
-			cout << "No monopoles with 1 wrapping in " << ii << "-th iteration" << endl;
-			cout << "Not considered " << ii << "-th iteration" << endl;
-			continue;
-		}
-
-		for (int kk = 0; kk < mean_rhok.size(); ++kk) {
-			mean_rhok[kk] /= n_conf;
-			mean_rhok_rho1[kk] /= n_conf;
-			mean0_2[kk] /= n_conf;
-			mean1_2[kk] /= n_conf;
-
-			err_rhok.push_back(mean0_2[kk] - mean_rhok[kk] * mean_rhok[kk]);
-			err_rhok_rho1.push_back(mean1_2[kk] - mean_rhok_rho1[kk] * mean_rhok_rho1[kk]);
-
-			err_rhok[kk] /= (n_conf - 1);
-			err_rhok_rho1[kk] /= (n_conf - 1);
-
-			if (err_rhok_rho1[kk] < 0) {
-				cout << "err_rhok_rho1[" << to_string(kk) << "] < 0" << endl;
-				err_rhok_rho1[kk] = 0;
-			}
-			else {
-				err_rhok_rho1[kk] = sqrt(err_rhok_rho1[kk]);
-			}
-		}
-
-		if (mean_rhok_rho1.size() > 1) {
-			mean_rhok_rho1.erase(mean_rhok_rho1.begin()); //non ci interessa rho_1/rho_1 = 1
-			err_rhok_rho1.erase(err_rhok_rho1.begin());
-		}
-		else {
-			cout << "No monopoles with more than 1 wrapping in " << ii << "-th iteration" << endl;
-			cout << "Not considered " << ii << "-th iteration" << endl;
-			continue;
-		}
-
-		int n_not_considered = 0;
-
-		for (int kk = 1; kk < mean_rhok.size(); ++kk) {
-
-			if (matrix_value_rhok[kk].empty() || matrix_value_rhok[0].empty()) {
-				cerr << "Warning: empty data for Bootstrap at k=" << kk << endl;
-				++n_not_considered;
-				continue;
-			}
-
-			vector <double> x1_vec, x2_vec;
-
-			for (int jj = 0; jj < min(matrix_value_rhok[kk].size(), matrix_value_rhok[0].size()); ++jj) {
-				if (matrix_value_rhok[0][jj] != 0) {
-					x1_vec.push_back(matrix_value_rhok[kk][jj]);
-					x2_vec.push_back(matrix_value_rhok[0][jj]);
-				}
-				else {
-					cout << "Omitted a zero rho_1 due to division issue" << endl;
-				}
-			}
-
-			mean_rhok_norm.push_back(0);
-			err_rhok_norm.push_back(0);
-			uniform_int_distribution<> dist_int(0, x1_vec.size() - 1);
-
-			if (bootstrap_2in(n_boot_steps, sampler, dist_int, f, x1_vec, x2_vec, &mean_rhok_norm[kk - 1 - n_not_considered], &err_rhok_norm[kk - 1 - n_not_considered]))
-			{
-				cout << "Error in Bootstrap for (ii, kk) = (" << to_string(ii) << ", " << to_string(kk) << endl;
-				return 1;
-			}
-
-			if (isnan(err_rhok_norm[kk - 1 - n_not_considered])) {
-				cout << "Encontered NaN in Bootstrap" << endl;
-				err_rhok_norm.resize(kk - 1 - n_not_considered, 0.0);;
-				break;
-			}
-			else if (err_rhok_norm[kk - 1 - n_not_considered] >= 0) {
-				err_rhok_norm[kk - 1 - n_not_considered] = sqrt(err_rhok_norm[kk - 1 - n_not_considered]);
-			}
-			else {
-				cout << "Encontered negative value in Bootstrap" << endl;
-				err_rhok_norm.resize(kk - 1 - n_not_considered, 0.0);;
-				break;
-			}
-			k_array.push_back(kk + 1);
-			x1_vec.clear();
-			x2_vec.clear();
-		}
-
-		//images:
-		name_image1 = "results/rhok_rho1_mpi" + mpi + "_" + to_string(ii + 1) + "thTemperature.png"; //<rho_k/rho_1>
-		name_image2 = "results/rhok_norm_mpi" + mpi + "_" + to_string(ii + 1) + "thTemperature.png"; //<rho_k>/<rho_1>
-
-		if (mean_rhok_norm.size() > 1) {
-
-			plot_points_errors(
-				k_array,
-				mean_rhok_rho1,
-				err_rhok_rho1,
-				name_image1,
-				title1,
-				y_name1,
-				pos_title1,
-				pos_y1,
-				heigh_y1,
-				width_canvas1
-			);
-
-			plot_points_errors(
-				k_array,
-				mean_rhok_norm,
-				err_rhok_norm,
-				name_image2,
-				title2,
-				y_name2,
-				pos_title2,
-				pos_y2,
-				heigh_y2,
-				width_canvas2
-			);
-		}
-		else {
-			cout << "No image generated for iteration n°" << ii << endl;
-			cout << "Not enough data points to produce a plot (need at least 2)." << endl;
-		}
-
-		//output_file:
-		name_output_file1 = "results/" + mpi + "_TempN" + to_string(ii + 1) + "_mean_rhok_rho1VSk.txt";
-		name_output_file2 = "results/" + mpi + "_TempN" + to_string(ii + 1) + "_mean_rhok_mean_rho1VSk.txt";
-
-		ofstream output_file1;
-		output_file1.open(name_output_file1);
-		if (!output_file1) {
-			cerr << "Error opening first output file" << endl;
-			return 1;
-		}
-
-		ofstream output_file2;
-		output_file2.open(name_output_file2);
-		if (!output_file2) {
-			cerr << "Error opening second output file" << endl;
-			return 1;
-		}
-
-		output_file1 << "#k \t <rhok/rho1> \t err(rhok/rho1):" << endl;
-		output_file2 << "#k \t <rhok>/<rho1> \t err(<rhok>/<rho1>):" << endl;
-
-		output_file1 << setprecision(numeric_limits<double>::max_digits10);
-		output_file2 << setprecision(numeric_limits<double>::max_digits10);
-
-		for (int kk = 0; kk < k_array.size(); ++kk) {
-			output_file1 << k_array[kk] << "\t" << mean_rhok_rho1[kk] << "\t" << err_rhok_rho1[kk] << endl;
-			output_file2 << k_array[kk] << "\t" << mean_rhok_norm[kk] << "\t" << err_rhok_norm[kk] << endl;
-		}
-
-		output_file1.close();
-		output_file2.close();
-
-		mean_rhok.clear();
-		mean_rhok_norm.clear();
-		mean_rhok_rho1.clear();
-		k_array.clear();
-		err_rhok.clear();
-		err_rhok_rho1.clear();
-		err_rhok_norm.clear();
 	}
+
+	//images:
+	name_image = "results/rho_tot_mpi" + mpi + ".png";
+	
+	if (err_rho_T3.size() > 1) {
+
+		plot_points_errors(
+			temp,
+			mean_rho_T3,
+			err_rho_T3,
+			name_image,
+			title,
+			y_name,
+			pos_title,
+			pos_y,
+			heigh_y,
+			width_canvas
+		);
+	}
+	else {
+		cout << "No image generated since:"<< endl;
+		cout << "Not enough data points to produce a plot (need at least 2)." << endl;
+	}
+
+	//output_file:
+
+	ofstream output_file;
+	output_file.open(name_output_file);
+	if (!output_file) {
+		cerr << "Error opening output file" << endl;
+		return 1;
+	}
+
+	output_file << "#T[MeV] \t <rho_tot/T^3>[adimensional] \t  \t error(rho_tot/T^3):" << endl;
+
+	output_file << setprecision(numeric_limits<double>::max_digits10);
+	
+	for (int ii = 0; ii < mean_rho_T3.size(); ++ii) {
+		output_file << temp[ii] << "\t" << mean_rho_T3[ii] << "\t" << err_rho_T3[ii] << endl;
+	}
+
+	output_file.close();
 
 	return 0;
 }
@@ -497,7 +355,7 @@ void minimal_read_file_LPC(
 
 	file_lpc.open(name_file_lpc);
 	if (!file_lpc) {
-		cerr << "Error opening file list" << endl;
+		cerr << "Error opening file lcp list" << endl;
 		return;
 	}
 
@@ -630,9 +488,9 @@ void plot_points_errors(
 	g_errors->SetMarkerColor(kBlack);
 	g_errors->SetMarkerStyle(20);
 	g_errors->SetTitle("");
-	g_errors->GetXaxis()->SetLimits(0, max_x + 1);
+	g_errors->GetXaxis()->SetLimits(min_x - 10, max_x + 10);
 	//g_errors->GetYaxis()->SetRangeUser(min_y - 0.01 * fabs(min_y), max_y + 0.01 * fabs(max_y));
-	g_errors->GetYaxis()->SetRangeUser(0, max_y + 0.01 * fabs(max_y));
+	g_errors->GetYaxis()->SetRangeUser(min_y - 0.01, max_y + 0.01);
 	g_errors->Draw("AP");
 
 	// 2. Graph with only the line joining the points:
